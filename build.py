cf17d4d766ee058bd1de7ae55eb87af9561fb4df
madd-games <madd.gods@gmail.com>
2014-12-18 13:16:14 +0000

    Update build.py

--- build.py
+++ build.py
@@ -1,2 +1,15 @@
 #! /usr/bin/python
-# Basically, run this script every time you modify any of tthe modules.
+# Basically, run this script every time you modify any of the modules.
+
+import sys, os
+
+f = open("auto/main.js", "wb")
+f.write("// automatically generated; don't touch this!\n")
+f.write("function initModules(){\n")
+
+for name in os.listdir("modules"):
+	if name.endswith(".js"):
+		modname = name[:-3]
+		f.write("\tinit%s();\n" % modname)
+
+f.close()
