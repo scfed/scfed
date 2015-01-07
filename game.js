d805ae0e3ba60a87d209ab37e3bb37329bedaf12
madd-games <madd.gods@gmail.com>
2015-01-07 12:43:48 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,2 +1,17 @@
 // game.js
-// main file.
+// Main file.
+// NOTE: I suggest you talk to Mariusz if you want to edit this.
+
+var gameCanvasContext;
+
+function gameLoop()
+{
+  bubblePaint(gameCanvasContext, 6, 6);
+  // TODO: call setTimeout again, once updates will actually be needed.
+};
+
+function gameLoad()
+{
+  gameCanvasContext = document.getElementById("game_canvas").getContext('2d');
+  setTimeout(gameLoop, 1);
+};
