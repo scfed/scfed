15683eacc718e404a53a7273733e16144b88b376
Mariusz Pilipczuk <projekt.darma@gmail.com>
2015-01-07 17:31:03 +0000

    Drawing the bubbles

--- bubbles.js
+++ bubbles.js
@@ -18,17 +18,17 @@ var BUBBLE_ROW_LENGTH = 6;
 // each row is an array of types.
 // TODO: make this dynamically generated, perhaps with a seed?
 var bubbleRows = [
-  [BUBBLE_PP, BUBBLE_PM, BUBBLE_TP, BUBBLE_TP, BUBBLE_TM, BUBBLE_TP],
-  [BUBBLE_PM, BUBBLE_PP, BUBBLE_TP, BUBBLE_MP, BUBBLE_TP, BUBBLE_TM],
-  [BUBBLE_TP, BUBBLE_TP, BUBBLE_MP, BUBBLE_PP, BUBBLE_PM, BUBBLE_PM]
+	[BUBBLE_PP, BUBBLE_PM, BUBBLE_TP, BUBBLE_TP, BUBBLE_TM, BUBBLE_TP],
+	[BUBBLE_PM, BUBBLE_PP, BUBBLE_TP, BUBBLE_PM, BUBBLE_TP, BUBBLE_TM],
+	[BUBBLE_TP, BUBBLE_TP, BUBBLE_PM, BUBBLE_PP, BUBBLE_PM, BUBBLE_PM]
 ]
 
 // images
 var bubbleImages = [
-  new Image(),
-  new Image(),
-  new Image(),
-  new Image()
+	new Image(),
+	new Image(),
+	new Image(),
+	new Image()
 ]
 
 bubbleImages[0].src = "img/bubble_pp.png";
@@ -39,17 +39,17 @@ bubbleImages[3].src = "img/bubble_tm.png";
 // draw the balls with the top-left corner at (x, y)
 function bubblePaint(ctx, x, y)
 {
-  var i;
-  var j;
-  
-  for (i=0; i<bubbleRows.length; i++)
-  {
-    for (j=0; j<6; j++)
-    {
-      var type = bubbleRows[i][j];
-      var img = bubbleImages[type];
-      
-      ctx.drawImage(img, x+BUBBLE_DIA*j, y+BUBBLE_DIA*i);
-    };
-  };
+	var i;
+	var j;
+	
+	for (i=0; i<bubbleRows.length; i++)
+	{
+		for (j=0; j<6; j++)
+		{
+			var type = bubbleRows[i][j];
+			var img = bubbleImages[type];
+			
+			ctx.drawImage(img, x+BUBBLE_DIA*j, y+BUBBLE_DIA*i);
+		};
+	};
 };
