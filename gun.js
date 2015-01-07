8d20c7610b78dbda1192ba09493e4a41e694fdb7
Vyrial <pieman975@gmail.com>
2015-01-07 12:38:22 +0000

    Scripts happenned.
    +images
    +functions
    
    -pointless commentary

--- gun.js
+++ gun.js
@@ -1,7 +1,16 @@
 //this is a gun.js At this moment in time I've got no idea what I'm doing
 
 //I need to make like, a rotating bit.
-var gunRotate = 0;
-
-//I also need a static part, which displays the BALL that is loaded.
-var gunStatic = 0;
+var gunAngle = 0;
+var gunBaseImage = new Image();
+var gunRotateImage = new Image();
+gunBaseImage.src = "img/gunBase.png"
+gunRotateImage.src = "img/gunRotate.png"
+function gunDraw(ctx, x, y)
+{
+  //DRAWING A GUN
+}
+function gunRotate()
+{
+  //ROTATING A GUN
+}
