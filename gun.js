65d97465cd746e12a1c372071d4875db8f072bbf
Vyrial <pieman975@gmail.com>
2015-01-07 12:30:11 +0000

    added some variables
    I have no idea what I'm doing.

--- gun.js
+++ gun.js
@@ -1,1 +1,7 @@
-//
+//this is a gun.js At this moment in time I've got no idea what I'm doing
+
+//I need to make like, a rotating bit.
+var gunRotate = 0;
+
+//I also need a static part, which displays the BALL that is loaded.
+var gunStatic = 0;
